d5f8a9cffa91103c0a364752ca84b28275e92340
Guilherme dos Santos <gs92dev@gmail.com>
2023-07-31 12:04:46 -0500

    Usinf for in objcts

---
+++ FrontEnd/de-structuring/index.js
@@ -0,0 +1,26 @@
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
+function testBracketsDynamicAccess() {
+    var dynamicKey;
+    if(Math.random() > 0.5) {
+      dynamicKey = "speed";
+     }else{
+       dynamicKey = "color";
+     }
+  
+      var drone = {
+        speed: 15,
+        color: "orange"
+      }
+  
+      console.log(drone[dynamicKey]);
+  }
+  testBracketsDynamicAccess();
