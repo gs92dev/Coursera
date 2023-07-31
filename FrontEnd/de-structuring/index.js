8bded433066c1cff131f6da7eb187c1a1f0b0c7e
Guilherme dos Santos <gs92dev@gmail.com>
2023-07-31 13:01:01 -0500

    finished exercice destructuring objects

--- FrontEnd/de-structuring/index.js
+++ FrontEnd/de-structuring/index.js
@@ -1,26 +1,66 @@
 var clothingItem = {
-    price: 50,
-    color: 'beige',
-    material: 'cotton',
-    season: 'autumn'
-}
+  price: 50,
+  color: "beige",
+  material: "cotton",
+  season: "autumn",
+};
 
-for( key of Object.keys(clothingItem) ) {
-    console.log(key, ":", clothingItem[key])
+for (key of Object.keys(clothingItem)) {
+  console.log(key, ":", clothingItem[key]);
 }
 function testBracketsDynamicAccess() {
-    var dynamicKey;
-    if(Math.random() > 0.5) {
-      dynamicKey = "speed";
-     }else{
-       dynamicKey = "color";
-     }
-  
-      var drone = {
-        speed: 15,
-        color: "orange"
-      }
-  
-      console.log(drone[dynamicKey]);
+  var dynamicKey;
+  if (Math.random() > 0.5) {
+    dynamicKey = "speed";
+  } else {
+    dynamicKey = "color";
   }
-  testBracketsDynamicAccess();
+
+  var drone = {
+    speed: 15,
+    color: "orange",
+  };
+
+  console.log(drone[dynamicKey]);
+}
+testBracketsDynamicAccess();
+// Task 1
+var dairy = [
+  "cheese",
+  "sour cream",
+  "milk",
+  "yogurt",
+  "ice cream",
+  "milkshake",
+];
+function logDairy() {
+  for (dairies of dairy) {
+    console.log(dairies);
+  }
+}
+logDairy();
+
+// Task 2
+const animal = {
+  canJump: true,
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+function birdCan() {
+  for (key of Object.keys(bird)) {
+    console.log(` ${key} : ${bird[key]}`);
+  }
+}
+birdCan();
+
+// Task 3
+function animalCan() {
+  for (const key in bird) {
+    console.log(key, ":", bird[key]);
+  }
+}
+animalCan();
