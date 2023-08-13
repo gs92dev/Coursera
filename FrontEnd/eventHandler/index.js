62b45e25f790396b35a66693f7f2fc54c0fbb3f6
Guilherme dos Santos <gs92dev@gmail.com>
2023-08-12 20:38:37 -0400

    Comments

--- FrontEnd/eventHandler/index.js
+++ FrontEnd/eventHandler/index.js
@@ -1,6 +1,8 @@
+//create a mew tag h1
 var h1 = document.createElement('h1')
+//insert the text into the tag element
 h1.innerText = "Type into the input to make this text change"
-
+// Create a input element
 var input = document.createElement('input')
 input.setAttribute('type', 'text')
 
