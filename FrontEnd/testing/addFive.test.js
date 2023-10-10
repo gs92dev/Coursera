df1a2a98f2992ce0eedd6aacf31ae3ca3949a08f
Guilherme dos Santos <gs92dev@gmail.com>
2023-10-09 20:38:45 -0500

    created first react

--- FrontEnd/testing/addFive.test.js
+++
@@ -1,5 +0,0 @@
-const addFive = require(`./index`);
-
-test('Add 5',()=>{
-    expect(addFive(2)).toBe(7)
-})
--- FrontEnd/testing/index.js
+++
@@ -1,6 +0,0 @@
-//Add five to a
-function addFive(a) {
-  return a + 5;
-}
-console.log(addFive(1));
-module.exports = addFive;
