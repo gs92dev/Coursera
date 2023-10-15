b24a79edf4e6976e605ecdbd612dd963ed98dcf5
Guilherme dos Santos <gs92dev@gmail.com>
2023-10-14 19:01:51 -0500

    Created Heading

--- FrontEnd/first_react/src/App.js
+++ FrontEnd/first_react/src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+function Header() {
+  return(
+    <h1>Hello World</h1>)
+}
+  
+  
+
 function App() {
-  return <div className="App"></div>;
+  return <Header />;
 }
 
 export default App;
