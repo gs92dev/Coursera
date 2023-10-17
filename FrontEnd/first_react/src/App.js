f47232df5ba93ad2127d358ba4a7d4cdcbf587b9
Guilherme dos Santos <gs92dev@gmail.com>
2023-10-16 20:15:34 -0500

    Added some cards

--- FrontEnd/first_react/src/App.js
+++ FrontEnd/first_react/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import { Heading } from "./Heading";
+import Card from "./Card";
 
-
-let firstName = ["Guilherme","Daniel","John"]
+let firstName = ["Guilherme", "Daniel", "John"];
 
 function App() {
-  return( 
-  <Heading firstName={firstName}/>
-  )
+  return (
+    <>
+      <Heading firstName={firstName} />
+      <Card h2="First card's h2" />
+
+      <Card h3="First card's h3" />
+    </>
+  );
 }
 
 export default App;
