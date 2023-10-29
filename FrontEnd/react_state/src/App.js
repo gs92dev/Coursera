bf4df664299366c4e9ee42d8df74d0f5a05f5170
Guilherme dos Santos <gs92dev@gmail.com>
2023-10-28 20:33:24 -0500

    Using react dom

--- FrontEnd/react_state/src/App.js
+++ FrontEnd/react_state/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Fruits from "./Fruits";
 import FruitsCounter from "./FruitsCounter";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   const [fruits] = React.useState([
-      {fruitName: 'apple', id: 1},
-      {fruitName: 'apple', id: 2},
-      {fruitName: 'plum', id: 3},
+    { fruitName: "apple", id: 1 },
+    { fruitName: "apple", id: 2 },
+    { fruitName: "plum", id: 3 },
   ]);
 
   return (
     <div className="App">
-      <h1>Where should the state go?</h1>
-      <Fruits fruits={fruits} />
-      <FruitsCounter fruits={fruits} />
+      <Link to="/">Home</Link>
+      <Link to="/fruits">What Fruits we have today</Link>
+      <Routes>
+        <Route path="/" element={<FruitsCounter fruits={fruits} />} />
+        <Route path="/fruits" element={<Fruits fruits={fruits} />} />
+      </Routes>
     </div>
   );
 }
