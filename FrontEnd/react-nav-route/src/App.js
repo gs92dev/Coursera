79dc8e7ee3aa12723d9da4cca8db95c7114fde74
Guilherme dos Santos <gs92dev@gmail.com>
2023-10-30 20:38:53 -0500

    New image

--- FrontEnd/react-nav-route/src/App.js
+++ FrontEnd/react-nav-route/src/App.js
@@ -3,22 +3,33 @@ import Homepage from "./Homepage";
 import AboutLittleLemon from "./AboutLittleLemon";
 import { Routes, Route, Link } from "react-router-dom";
 import Contact from "./Contact";
+import Image from "./Image";
 
 function App() {
   return (
-    <div> 
-	  <nav>
-      <Link to="/" className="nav-item">Homepage</Link>
-        <Link to="/about" className="nav-item">About Little Lemon</Link>
-        <Link to="/contact" className="nav-item">Contact</Link>
-	  </nav>
-      <Routes> 
+    <div>
+      <nav>
+        <Link to="/" className="nav-item">
+          Homepage
+        </Link>
+        <Link to="/about" className="nav-item">
+          About Little Lemon
+        </Link>
+        <Link to="/contact" className="nav-item">
+          Contact
+        </Link>
+        <Link to="/image" className="nav-item">
+          Image
+        </Link>
+      </nav>
+      <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<AboutLittleLemon />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/image" element={<Image />}></Route>
       </Routes>
     </div>
   );
-};
+}
 
 export default App;
---
+++ FrontEnd/react-nav-route/src/Image.js
@@ -0,0 +1,4 @@
+import image from "./assets/logo.png";
+export default function Image() {
+  return <img src={image} width="500px" alt="New image" />;
+}
