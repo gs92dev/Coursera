f865d04cbbd50530145db8435dfc146f446bbb5d
Guilherme dos Santos <gs92dev@gmail.com>
2023-10-31 22:21:48 -0500

    included audio

--- FrontEnd/react-nav-route/src/App.js
+++ FrontEnd/react-nav-route/src/App.js
@@ -4,17 +4,9 @@ import AboutLittleLemon from "./AboutLittleLemon";
 import { Routes, Route, Link } from "react-router-dom";
 import Contact from "./Contact";
 import Image from "./Image";
-import ReactPlayer from "react-player";
+import Audio from "./Audio";
 
 function App() {
-  const bird1 = new Audio(
-    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
-  );
-
-  const bird2 = new Audio(
-    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
-  );
-
   return (
     <div>
       <nav>
@@ -30,12 +22,16 @@ function App() {
         <Link to="/image" className="nav-item">
           Image
         </Link>
+        <Link to="/audio" className="nav-item">
+          Audio
+        </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<AboutLittleLemon />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/image" element={<Image />}></Route>
+        <Route path="/audio" element={<Audio />}></Route>
       </Routes>
     </div>
   );
