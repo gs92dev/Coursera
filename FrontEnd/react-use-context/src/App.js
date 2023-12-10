f6e1727c0d006f7be99eefe43396395fef36f454
Guilherme dos Santos <gs92dev@gmail.com>
2023-12-09 19:45:36 -0600

    Use context

--- FrontEnd/react-use-context/src/App.js
+++ FrontEnd/react-use-context/src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
+import { UserProvider, useUser } from "./UserContext";
+import React from "react";
+
+const Hello = () => {
+  const { user } = useUser();
+  return <p>{`Hello there ${user.name}`}</p>;
+};
 
 function App() {
+  const user = useUser();
+  console.log(user);
   return (
     <div className="App">
       <p>Hello world</p>
+      <UserProvider>
+        <Hello />
+        <Hello />
+      </UserProvider>
     </div>
   );
 }
-
-export default App;
+const Root = () => {
+  return (
+    <>
+      <App />
+    </>
+  );
+};
+export default Root;
--- FrontEnd/react-use-context/src/UserContext.js
+++ FrontEnd/react-use-context/src/UserContext.js
@@ -1,3 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState, useContext } from "react";
 
 const UserContext = createContext(undefined);
+
+export const UserProvider = ({ children }) => {
+  const [user] = useState({
+    name: "John",
+    email: "john@email.com",
+    dob: "01/01/200",
+  });
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
+};
+export const useUser = () => useContext(UserContext);
